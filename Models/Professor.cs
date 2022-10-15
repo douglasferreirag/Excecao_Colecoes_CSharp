9306242053a426c96c273339392aedc49b0b7a4f
Douglas <douglas.ferreirag@gmail.com>
2022-10-14 22:32:23 -0300

    Tratando uma excecao

---
+++ Models/Professor.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HerancaPolimorfismo.Models
+{
+    public class Professor : Pessoa
+    {
+        public decimal Salario { get; set; }
+
+         public override void Apresentar(){
+
+                Console.WriteLine($"Olá, meu nome é {Nome}, tenho {Idade} anos e sou um professor  e ganho {Salario} reais");
+            }
+
+    }
+}
